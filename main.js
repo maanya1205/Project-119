99556bdd29a43bf67cc97fc85d747c0f4e13cbb0
maanya1205 <77110811+maanya1205@users.noreply.github.com>
2021-08-03 09:27:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){}
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+    classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/sfr_SrcBV/model.json",modelLoaded);
+}
+function modelLoaded(){
+console.log("MODEL IS LOADED!!");
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML= results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML= results[0].confidence.toFixed(3);
+    }
+}
